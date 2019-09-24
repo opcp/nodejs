375768960f711944e021f8c1232c825100c8a212
Shan Yi <a24215188@gmail.com>
2019-09-24 13:37:45 +0800

    0924

--- my_babel/src/index.js
+++ my_babel/src/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const url = require('url')
 const body_parser = require('body-parser')
 const app = express()
+const multer =require('multer')
+const upload = multer({dest:'tmp_upload/'})
+const fs = require('fs');
 
 // const urlencodeparser=body_parser.urlencoded({extended:false});
 
 app.use(body_parser.urlencoded({extended:false}))
+app.use(body_parser.json())
 
 app.set('view engine','ejs')
 
@@ -39,7 +43,42 @@ app.post('/try_post_form',(req,response)=>{
     // response.send(JSON.stringify(req.body))
 })
 
-app.use((require,response)=>{
+app.get('/try_post_form2', (req, response)=>{
+    response.send('get: try-post-form2');
+});
+app.post('/try_post_form2', (req, response)=>{
+    response.json(req.body);
+
+    //res.send(JSON.stringify(req.body));
+});
+app.put('/try_post_form2', (req, response)=>{
+    response.send("PUT: try-post-form2");
+});
+
+app.post('/try_upload',upload.single('avatar'),(req, response)=>{
+    if(req.file.originalname && req.file){
+        console.log(req.file)
+
+        switch(req.file.mimetype){
+            case 'image/png' :
+            case 'image/jpeg' :
+                fs.createReadStream(req.file.path)
+                    .pipe(
+                        fs.createWriteStream('public/img' + req.file.originalname)
+                    );
+                    response.send('ok')
+                    break;
+            default:
+               return response.send('not filetype')
+        }
+    }
+    else{
+        response.send('上傳失敗')
+    }
+})
+
+
+app.use((req,response)=>{
     response.type('text/plain')
     response.status(404);
     response.send(`404 找不到頁面喔`)
